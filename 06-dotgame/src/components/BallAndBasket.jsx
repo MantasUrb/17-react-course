58d47a337aa05663ecebaedd8677906c3904bac7
Mantas Urbonas <mantasu11@gmail.com>
2022-01-17 14:10:18 +0200

    child component changed to functional hooks

--- 06-dotgame/src/components/BallAndBasket.jsx
+++ 06-dotgame/src/components/BallAndBasket.jsx
@@ -1,73 +1,69 @@
-import { Component } from 'react';
+import { useState, useEffect } from "react";
 
-class BallAndBasket extends Component {
+function BallAndBasket(props) {
+  const randomCoordinatesX = () => Math.floor(Math.random() * 96.5) + "%";
+  const randomCoordinatesY = () => Math.floor(Math.random() * 94) + "%";
 
-    randomCoordinatesX = () => Math.floor(Math.random() * 96.5) + "%";
-    randomCoordinatesY = () => Math.floor(Math.random() * 94) + "%";
+  const [basketTop, setBasketTop] = useState(randomCoordinatesY);
+  const [basketLeft, setBasketLeft] = useState(randomCoordinatesX);
+  const [score, setScore] = useState(0);
 
-    constructor() {
-        super();
-        this.state = {
-            basketTop: this.randomCoordinatesY(),
-            basketLeft: this.randomCoordinatesX(),
-            score: 0,
-        }
+  const changeBasketTop = () => {
+    setBasketTop(randomCoordinatesY);
+  };
+
+  const changeBasketLeft = () => {
+    setBasketLeft(randomCoordinatesX);
+  };
+
+  const add = (countIsNow) => {
+    const res = countIsNow + 1;
+    return res;
+  };
+
+  function alertWhenScore() {
+    const basket = document.getElementsByClassName("basket");
+    const ball = document.getElementsByClassName("ball");
+
+    let basketX = basket[0].offsetLeft;
+    let basketY = basket[0].offsetTop;
+    let ballX = ball[0].offsetLeft;
+    let ballY = ball[0].offsetTop;
+
+    if (
+      (ballX === basketX + 10 || ballX === basketX + 20) &&
+      (ballY === basketY + 15 || ballY === basketY + 21)
+    ) {
+      alert("SCORE !!! SCORE !!! SCORE !!!");
+      changeBasketTop();
+      changeBasketLeft();
+      setScore(add);
     }
+  }
 
-    add = (function () {
-        let counter = 0;
-        return function () {counter += 1; return counter}
-    })();
+  useEffect(() => {
+    alertWhenScore();
+  });
 
-    alertWhenScore () {
-        const basket = document.getElementsByClassName("basket");
-        const ball = document.getElementsByClassName("ball");
-
-        let basketX = basket[0].offsetLeft;
-        let basketY = basket[0].offsetTop;
-        let ballX = ball[0].offsetLeft;
-        let ballY = ball[0].offsetTop;
-
-        if (((ballX === basketX + 10 || ballX === basketX + 20) && 
-            (ballY === basketY + 15 || ballY === basketY + 21))) {
-            return (
-                alert("SCORE !!! SCORE !!! SCORE !!!"),
-                this.setState({
-                    basketTop: this.randomCoordinatesY(),
-                    basketLeft: this.randomCoordinatesX(),
-                    score: this.add(),
-                })
-            )
-        }
-    }
-
-    componentDidUpdate () {
-        this.alertWhenScore();
-    }
-
-    render() {
-        return (
-            <>
-            <span className="score">{this.state.score}</span>
-            <div 
-                className="ball"
-                style={{
-                    bottom: this.props.bottomvalue + "%",
-                    left: Math.abs(this.props.leftvalue) + "%"
-                }}
-            >
-            </div>
-            <div 
-                className="basket"
-                style={{
-                    top: this.state.basketTop,
-                    left: this.state.basketLeft,
-                }}
-            >
-            </div>
-            </>
-        )
-    }
+  return (
+    <>
+      <span className="score">{score}</span>
+      <div
+        className="ball"
+        style={{
+          bottom: props.bottomvalue + "%",
+          left: Math.abs(props.leftvalue) + "%",
+        }}
+      ></div>
+      <div
+        className="basket"
+        style={{
+          top: basketTop,
+          left: basketLeft,
+        }}
+      ></div>
+    </>
+  );
 }
 
-export default BallAndBasket ;
+export default BallAndBasket;
