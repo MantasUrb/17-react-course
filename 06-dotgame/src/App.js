15634792e202a74a92442a612bfb5bf605ec85cc
Mantas Urbonas <mantasu11@gmail.com>
2022-01-17 11:20:57 +0200

    app.js changed from class to functional hooks

--- 06-dotgame/src/App.js
+++ 06-dotgame/src/App.js
@@ -1,60 +1,50 @@
 import './App.css';
 import { Slider } from '@mui/material';
-import { Component } from 'react';
+import { useState } from 'react';
 import BallAndBasket from "./components/BallAndBasket"
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-        sliderHorzValue: 50,
-        sliderVertValue: 50,
-    }
-}
+function App () {
 
-handleChangeHorizontal = value => {
-    this.setState({
-        sliderHorzValue: value.target.value,
-    })
-}
+  const [sliderHorzValue, setHorzValue] = useState(50);
+  const [sliderVertValue, setVertValue] = useState(50);
 
-handleChangeVertical = value => {
-    this.setState({
-        sliderVertValue: value.target.value,
-    })
-}
-
-render() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <div className="main-container">
-          <BallAndBasket
-              leftvalue={-this.state.sliderHorzValue}
-              bottomvalue={this.state.sliderVertValue}
-          />
-        </div>
-        <div className="slider-horizontal">
-          <Slider 
-            min={1}
-            max={99}
-            defaultValue={50}
-            onChange={this.handleChangeHorizontal}
-          />
-        </div>
-        <div className="slider-vertical">
-          <Slider 
-            min={-1}
-            max={96}
-            defaultValue={50} 
-            orientation="vertical"
-            onChange={this.handleChangeVertical}
-          />
-        </div>
-      </header>
-    </div>
-    );
+  const handleChangeHorizontal = value => {
+    setHorzValue(value.target.value);
   }
+
+  const handleChangeVertical = value => {
+    setVertValue(value.target.value);
+  }
+
+    return (
+          <div className="App">
+            <header className="App-header">
+              <div className="main-container">
+                <BallAndBasket
+                    leftvalue={-sliderHorzValue}
+                    bottomvalue={sliderVertValue}
+                />
+              </div>
+              <div className="slider-horizontal">
+                <Slider 
+                  min={1}
+                  max={99}
+                  defaultValue={50}
+                  onChange={handleChangeHorizontal}
+                />
+              </div>
+              <div className="slider-vertical">
+                <Slider 
+                  min={-1}
+                  max={96}
+                  defaultValue={50} 
+                  orientation="vertical"
+                  onChange={handleChangeVertical}
+                />
+              </div>
+            </header>
+          </div>
+          );
 }
 
 export default App;
--- 07-hooks/src/App.js
+++ 07-hooks/src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
-// import Message from './components/Message'
-import Effect from './components/Effect'
+import Message from './components/Message'
+// import Effect from './components/Effect'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Effect vardas1={"Michael"} vardas2={"Jessy"}/>
+        {/* <Effect vardas1={"Michael"} vardas2={"Jessy"}/> */}
+        <Message/>
       </header>
     </div>
   );
